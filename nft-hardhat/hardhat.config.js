019930e4da239ee5038c8e750658579bebb911de
SweetBubalehj <573567@mail.ru>
2023-03-20 18:15:27 +0200

    first commit

---
+++ nft-hardhat/hardhat.config.js
@@ -0,0 +1,53 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  defaultNetwork: "testnet",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    hardhat: {},
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [ process.env.PRIVATE_KEY ],
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [ process.env.PRIVATE_KEY ],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://bscscan.com/
+    apiKey: "7U8E37949EA9FJGJKYERNEAVMAD8BS693A"
+  },
+  mocha: {
+    timeout: 20000,
+  },
+};
---
+++ nft-hardhat/scripts/deploy.js
@@ -0,0 +1,25 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const MyERC721 = await hre.ethers.getContractFactory("MyERC721");
+  const myERC = await MyERC721.deploy();
+
+  await myERC.deployed();
+
+  console.log(
+    `ERC721 deployed to ${myERC.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
